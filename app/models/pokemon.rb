9c76228d53bbf7b5d0cdcf486a1dae9ffb98020a
Brian Wong <brianw7218@gmail.com>
2024-10-07 10:15:06 -0700

    Add Pokemon model

---
+++ app/models/pokemon.rb
@@ -0,0 +1,7 @@
+class Pokemon < ApplicationRecord
+  self.table_name = "pokemon"
+  self.primary_key = "id"
+
+  belongs_to :type_1, class_name: "PokemonType", foreign_key: "type_1_id"
+  belongs_to :type_2, class_name: "PokemonType", foreign_key: "type_2_id"
+end
