064f9b00b66624e8671bd22ce85f184fe8cff356
Brian Wong <brianw7218@gmail.com>
2024-10-08 10:20:13 -0700

    Fix n+1 problem when querying Pokemon stock pairs

--- app/graphql/types/pokemon_stock_pair_type.rb
+++ app/graphql/types/pokemon_stock_pair_type.rb
@@ -4,5 +4,17 @@ module Types
     field :pokemon, Types::PokemonType, null: false
     field :stock, Types::StockType, null: false
     field :season, Types::SeasonType, null: false
+
+    def pokemon
+      dataloader.with(Sources::ActiveRecord, ::Pokemon).load(object.pokemon_id)
+    end
+
+    def stock
+      dataloader.with(Sources::ActiveRecord, ::Stock).load(object.stock_id)
+    end
+
+    def season
+      dataloader.with(Sources::ActiveRecord, ::Season).load(object.season_id)
+    end
   end
 end
